c9152647ea1d6958d9d2f536ca3f0609ab0cfd49
Eugene_K <41466743+Jeka38@users.noreply.github.com>
2026-05-08 14:43:54 +0800

    Add StartUI launcher shim to fix manifest merge crash

---
+++ src/main/java/de/monocles/mod/ui/StartUI.java
@@ -0,0 +1,9 @@
+package de.monocles.mod.ui;
+
+import eu.siacs.conversations.ui.WelcomeActivity;
+
+/**
+ * Launcher entry activity kept for manifest compatibility.
+ */
+public class StartUI extends WelcomeActivity {
+}
